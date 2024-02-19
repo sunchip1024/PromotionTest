e2366c59ebefa3100250f0c017a9c9d51d163be9
utility1024 <lsj010320@inha.edu>
2024-02-19 18:00:13 +0900

    change promise

--- src/scripts/Headphone.js
+++ src/scripts/Headphone.js
@@ -2,36 +2,55 @@ import * as THREE from "three";
 import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 
+function loadMaterial({url}) {
+    const loader = new MTLLoader();
+
+    return new Promise((resolve, reject) => {
+        loader.load({url: url, onLoad: resolve, onError: reject});
+    });
+}
+
+function loadObject({url, material}) {
+    const loader = new OBJLoader();
+    loader.setMaterials(material);
+
+    return new Promise((resolve, reject) => {
+        loader.load({url: url, onLoad: resolve, onError: reject});
+    });
+} 
+
+function setTransform({object, scene, position = undefined, rotation = undefined, scale = undefined}) {
+    const mesh = new THREE.Object3D();
+    mesh.add(object);
+
+    if(position !== undefined) {
+        mesh.position.set(position.x, position.y, position.z);
+    }
+
+    if(scale !== undefined) {
+        mesh.scale.set(scale.x, scale.y, scale.z);
+    }
+
+    if(rotation !== undefined) {
+        mesh.rotation.x = rotation.x;
+        mesh.rotation.y = rotation.y;
+        mesh.rotation.z = rotation.z;
+    }
+
+    scene.add(mesh);
+
+    return new Promise((resolve, reject) => mesh);
+}
+
 export class HeadphoneOne {
-    constructor({ scene, position = undefined, rotation = undefined, scale = undefined}) {
-        new MTLLoader()
-            .setPath("./models/headphone_1/")
-            .load("headphone_1.mtl", ( materials ) => {
-                materials.preload();
-                new OBJLoader()
-                    .setMaterials( materials )
-                    .setPath("./models/headphone_1/")
-                    .load("headphone_1.obj", ( object ) => {
-                        this.mesh = new THREE.Object3D();
-                        this.mesh.add(object);
+    constructor({ scene, position = undefined, rotation = undefined, scale = undefined }) {
+        const materialUrl = "./models/headphone_1/headphone_1.mtl";
+        const modelUrl = "./models/headphone_1/headphone_1.obj";
 
-                        if(position !== undefined) {
-                            this.mesh.position.set(position.x, position.y, position.z);
-                        }
-
-                        if(scale !== undefined) {
-                            this.mesh.scale.set(scale.x, scale.y, scale.z);
-                        }
-
-                        if(rotation !== undefined) {
-                            this.mesh.rotation.x = rotation.x;
-                            this.mesh.rotation.y = rotation.y;
-                            this.mesh.rotation.z = rotation.z;
-                        }
-
-                        scene.add(this.mesh);
-                    });
-            });
+        loadMaterial(materialUrl)
+            .then(( material ) => loadObject(modelUrl, material))
+            .then(( object ) => setTransform(object, scene, position, rotation, scale))
+            .catch(( err ) => console.error(err));
     }
 }
 
